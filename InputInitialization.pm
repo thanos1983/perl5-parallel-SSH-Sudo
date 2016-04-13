f947ba43a870acb04384ca37ec7514341082d378
thanos1983 <thanos.egw@gmail.com>
2016-04-14 00:02:18 +0200

    Adiing configuration file validation.

--- InputInitialization.pm
+++ InputInitialization.pm
@@ -1,9 +1,15 @@
 package InputInitialization;
 
+use constant {
+    FALSE  => 0,
+    TRUE   => 1,
+};
+
+
 sub new {
     my $class = shift;
     my $self = {
-        _configurationFile => shift,
+        _configurationFile => shift || undef,
     };
     bless $self, $class;
     return $self;
@@ -16,11 +22,12 @@ sub getConfFile {
 
 sub validateAndExtractData {
     my ( $self, $conf_file ) = @_;
-    $self->{_configurationFile} = $conf_file if defined($conf_file);
-    # if ($self->{_configurationFile} !~ /\.ini$/i) {
-    # return 'Please enter a valid file with .ini extension\n';
-    # }
-    return $self->{_configurationFile};
+    $conf_file = $self->{_configurationFile} if not defined($conf_file);
+
+    if ($conf_file !~ /\.ini$/i and $conf_file !~ /\.cfg$/i) {
+	return "Please enter a valid file with .ini extension or .cfg\n", $conf_file;
+    }
+    return FALSE, $conf_file;
 }
 
 1;
--- main.pl
+++ main.pl
@@ -5,10 +5,9 @@ use warnings;
 use InputInitialization;
 use Data::Dumper qw(Dumper);
 
-die "Please use correct input: 'perl $0 [Configuration File.ini]'\n" if @ARGV > 1;
-
-my $object = new InputInitialization($ARGV[0]);
+my $object = new InputInitialization('conf.txt');
+my ($error, $data) = $object->validateAndExtractData();
+print $error if ($error);
 # Get configuration file which is set using constructor.
-my $conf_file = $object->getConfFile();
-
-print Dumper \$conf_file;
+# my $conf_file = $object->getConfFile();
+print Dumper \$data;
